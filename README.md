f65901ba862fe0ccab7fc40dec48e23602e1c6a2
abdullahsheikh01 <abdullah6601@gmail.com>
2024-03-20 23:57:33 +0500

    Pushing my repo to git hub

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Simple_Command_Line_Calculator" 
---
+++ index.ts
@@ -0,0 +1,37 @@
+#! /usr/bin/env node
+import inquirer from "inquirer"
+
+const calc = await inquirer.prompt([
+    {
+        message : "Enter First Number to perform operation",
+        type : "number",
+        name : "firstNumber"
+    },
+    {
+        message : "Enter Second Number to perform operation",
+        type : "number",
+        name : "secondNumber"
+    },
+    {
+        message : "Select your operator",
+        type : "list",
+        name : "operator",
+        choices : ["Addition","Subtraction","Division","Multiplication"]
+    }
+]
+)
+if (calc.operator==="Addition"){
+    console.log(calc.firstNumber+calc.secondNumber)
+}
+else if (calc.operator==="Subtraction"){
+    console.log(calc.firstNumber-calc.secondNumber)
+}
+else if (calc.operator==="Multiplication"){
+    console.log(calc.firstNumber*calc.secondNumber)
+}
+else if (calc.operator==="Division"){
+    console.log(calc.firstNumber/calc.secondNumber)
+}
+else {
+    console.log("Invalid operator found!")
+}
